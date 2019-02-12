d8692f401130b4974e8ab7a95931cb76ba7b9657
Sakib <sasraf0@gmail.com>
2019-02-12 13:05:05 -0500

    2/12/19

--- Bad Dream/Assets/Scripts/Player/PlayerMovement.cs
+++ Bad Dream/Assets/Scripts/Player/PlayerMovement.cs
@@ -2,5 +2,29 @@
 
 public class PlayerMovement : MonoBehaviour
 {
-    
+    public float speed = 6f;
+
+    Vector3 movement;
+    Animator anim;
+    Rigidbody playerRigidBody;
+    int floormask;
+    float camRayLength = 100f;
+
+    private void Awake()
+    {
+        floormask = LayerMask.GetMask("Floor");
+        anim = GetComponent<Animator>();
+        playerRigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+    }
+
+    void Move(float h, float v)
+    {
+        movement.Set(h, 0f, v);
+    }
 }
