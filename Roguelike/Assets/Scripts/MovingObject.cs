d7e7a25987551eba9e6f6498022b8c8d37cdea10
Sakib <sasraf0@gmail.com>
2019-04-04 13:02:15 -0400

    started work on roguelike game

---
+++ Roguelike/Assets/Scripts/MovingObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MovingObject : MonoBehaviour
+{
+
+    public float moveTime = 0.1f;
+    public LayerMask blockingLayer;
+
+    private BoxCollider2D boxCollider;
+    private RigidBody2d rb2D;
+    private float inverseMoveTime;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
