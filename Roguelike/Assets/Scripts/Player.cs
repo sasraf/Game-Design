ad61d7c747fcb45e96603c643696499c1df2ae75
Sakib <sasraf0@gmail.com>
2019-04-12 11:51:40 -0400

    Revert "Update Player.cs"
    This reverts commit 63c1c947af444695915b77447c1f046f1480a7c6.

--- Roguelike/Assets/Scripts/Player.cs
+++ Roguelike/Assets/Scripts/Player.cs
@@ -1,209 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using System.Collections;
-using UnityEngine.UI;	
-using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-namespace Completed
+public class Player : MovingObject
 {
-	public class Player : MovingObject
-	{
-		public float restartLevelDelay = 1f;	
-		public int pointsPerFood = 10;			
-		public int pointsPerSoda = 20;
-		public int wallDamage = 1;					
-		public Text foodText;				
-		public AudioClip moveSound1;			
-		public AudioClip moveSound2;		
-		public AudioClip eatSound1;				
-		public AudioClip eatSound2;				
-		public AudioClip drinkSound1;			
-		public AudioClip drinkSound2;			
-		public AudioClip gameOverSound;			
-		
-		private Animator animator;				
-		private int food;      
-		
-#if UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
-        private Vector2 touchOrigin = -Vector2.one;	
-#endif
-		
-		
+    public int wallDamage = 1;
+    public int pointsPerFood = 10;
+    public int pointsPerSoda = 20;
+    public float restartLevelDelay = 1f;
+    public Text foodText;
 
-		protected override void Start ()
-		{
-	
-			animator = GetComponent<Animator>();
-			
+    private Animator animator;
+    private int food;
 
-			food = GameManager.instance.playerFoodPoints;
-			
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        animator = GetComponent<Animator>();
+        food = GameManager.instance.playerFoodPoints;
+        foodText.text = "Food " + foodText;
+        base.Start();
+    }
 
-			foodText.text = "Food: " + food;
-			
+    private void OnDisable()
+    {
+        GameManager.instance.playerFoodPoints = food;
+    }
 
-			base.Start ();
-		}
-		
-		
+    // Update is called once per frame
+    void Update()
+    {
+        if (!GameManager.instance.playersTurn) return;
 
-		private void OnDisable ()
-		{
+        int horizontal = 0;
+        int vertical = 0;
 
-			GameManager.instance.playerFoodPoints = food;
-		}
-		
-		
-		private void Update ()
-		{
+        horizontal = (int)(Input.GetAxisRaw("Horizontal"));
+        vertical = (int)(Input.GetAxisRaw("Vertical"));
 
-			if(!GameManager.instance.playersTurn) return;
-			
-			int horizontal = 0;  	
-			int vertical = 0;		
-			
+        if (horizontal != 0)
+        {
+            vertical = 0;
+        }
 
-#if UNITY_STANDALONE || UNITY_WEBPLAYER			
+        if (horizontal != 0 || vertical != 0)
+        {
+            AttemptMove<Wall>(horizontal, vertical);
+        }
 
-			horizontal = (int) (Input.GetAxisRaw ("Horizontal"));
-			
-			vertical = (int) (Input.GetAxisRaw ("Vertical"));
-			
-			if(horizontal != 0)
-			{
-				vertical = 0;
-			}
-			
-#elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
-			
-			if (Input.touchCount > 0)
-			{
-				Touch myTouch = Input.touches[0];
-				
-				if (myTouch.phase == TouchPhase.Began)
-				{
-					touchOrigin = myTouch.position;
-				}
-				
-				else if (myTouch.phase == TouchPhase.Ended && touchOrigin.x >= 0)
-				{
-					Vector2 touchEnd = myTouch.position;
-					
-					float x = touchEnd.x - touchOrigin.x;
-					
-					float y = touchEnd.y - touchOrigin.y;
-					
-					touchOrigin.x = -1;
-					
-					if (Mathf.Abs(x) > Mathf.Abs(y))
-						horizontal = x > 0 ? 1 : -1;
-					else
-						vertical = y > 0 ? 1 : -1;
-				}
-			}
-			
-#endif 
-			if(horizontal != 0 || vertical != 0)
-			{
-				AttemptMove<Wall> (horizontal, vertical);
-			}
-		}
-		
-		protected override void AttemptMove <T> (int xDir, int yDir)
-		{
-			food--;
-			
-			foodText.text = "Food: " + food;
-			
-			base.AttemptMove <T> (xDir, yDir);
-			
-			RaycastHit2D hit;
-			
-			if (Move (xDir, yDir, out hit)) 
-			{
-				SoundManager.instance.RandomizeSfx (moveSound1, moveSound2);
-			}
-			
-			CheckIfGameOver ();
-			
-			GameManager.instance.playersTurn = false;
-		}
-		
-		
-		protected override void OnCantMove <T> (T component)
-		{
+    }
 
-			Wall hitWall = component as Wall;
-			
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Exit") 
+        {
+            Invoke("Restart", restartLevelDelay);
+            enabled = false;
+        }
+        else if (other.tag == "Food")
+        {
+            food += pointsPerFood;
+            foodText.text = "+" + pointsPerFood + " Food: " + food;
+            other.gameObject.SetActive(false);
+        }
+        else if (other.tag == "Soda")
+        {
+            food += pointsPerSoda;
+            foodText.text = "+" + pointsPerSoda + " Food: " + food;
+            other.gameObject.SetActive(false);
+        }
+    }
 
-			hitWall.DamageWall (wallDamage);
-			
+    protected override void OnCantMove<T>(T component)
+    {
+            Wall hitWall = component as Wall;
+            hitWall.DamageWall(wallDamage);
+            animator.SetTrigger("playerChop");
+    }
 
-			animator.SetTrigger ("playerChop");
-		}
-		
-		
+    private void Restart()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
 
-		private void OnTriggerEnter2D (Collider2D other)
-		{
-			if(other.tag == "Exit")
-			{
-				Invoke ("Restart", restartLevelDelay);
-				
-				enabled = false;
-			}
-			
-			else if(other.tag == "Food")
-			{
-				food += pointsPerFood;
-				
-				foodText.text = "+" + pointsPerFood + " Food: " + food;
-				
-				SoundManager.instance.RandomizeSfx (eatSound1, eatSound2);
-				
-				other.gameObject.SetActive (false);
-			}
-			
-			else if(other.tag == "Soda")
-			{
-				food += pointsPerSoda;
-				
-				foodText.text = "+" + pointsPerSoda + " Food: " + food;
-				
-				SoundManager.instance.RandomizeSfx (drinkSound1, drinkSound2);
-				
-				other.gameObject.SetActive (false);
-			}
-		}
-		
-		
-		private void Restart ()
-		{
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
-		}
-		
-		
-		public void LoseFood (int loss)
-		{
-			animator.SetTrigger ("playerHit");
-			
-			food -= loss;
-			
-			foodText.text = "-"+ loss + " Food: " + food;
-			
-			CheckIfGameOver ();
-		}
-		
+    public void LoseFood(int loss)
+    {
+        animator.SetTrigger("playerHit");
+        food -= loss;
+        foodText.text = "-" + loss + " Food: " + food;
+        CheckIfGameOver();
+    }
 
-		private void CheckIfGameOver ()
-		{
-			if (food <= 0) 
-			{
-				SoundManager.instance.PlaySingle (gameOverSound);
-				
-				SoundManager.instance.musicSource.Stop();
-				
-				GameManager.instance.GameOver ();
-			}
-		}
-	}
+    protected override void AttemptMove<T>(int xDir, int yDir)
+    {
+        food--;
+        foodText.text = "Food " + foodText;
+
+        base.AttemptMove<T>(xDir, yDir);
+
+        RaycastHit2D hit;
+
+        CheckIfGameOver();
+        GameManager.instance.playersTurn = false;
+    }
+
+    private void CheckIfGameOver()
+    {
+        if (food <= 0)
+        {
+            GameManager.instance.GameOver();
+        }
+    }
+
 }
+
